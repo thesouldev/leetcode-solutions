1580ba869c9b57ffd6c3e5d727de0df2d3b1772b
Sasidharan V <52165229+thesouleddev@users.noreply.github.com>
2022-05-15 00:10:25 +0530

    updated bsearch problem

---
+++ 744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,21 @@
+from tarfile import TarError
+from tkinter.messagebox import NO
+
+
+class Solution:
+    def nextGreatestLetter(self, letters, target: str) -> str:
+        if target >= letters[-1]: 
+            return letters[0]
+        l, r = 0, len(letters) - 1
+        while l < r:
+            m = (l+r) // 2
+            if letters[m] > target:
+                r = m
+            else:
+                l = m + 1
+        return letters[l]
+
+        
+arr, target =  ["c","f","j"],  "a"
+obj = Solution()
+print(obj.nextGreatestLetter(arr, target))
